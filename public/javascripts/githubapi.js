5dddf02f6075aa4c69ed0b4c03701282e80efab6
Carlos Luis <clburguete@gmail.com>
2017-05-10 16:33:47 +0200

    user info processed

--- public/javascripts/githubapi.js
+++ public/javascripts/githubapi.js
@@ -6,28 +6,31 @@ class GitHubApi {
     }
 
     getUserInfo(username) {
+      return new Promise ((resolve,reject)=>{
         $.ajax({
             url: this.BASE_URL + username,
             method: "GET",
 
             success: function(response) {
                 $('.user-info').remove();
-                let keys = Object.keys(response);
-                keys.forEach(function(l) {
-                    if (response[l] !== null) {
-                        $('#repositories').append(`<p class='user-info'>${l} ${response[l]}</p>`);
-                    }
-
-
-                });
+                //let keys = Object.keys(response);
+                // keys.forEach(function(l) {
+                //      if (response[l] !== null) {
+                //         $('#repositories').append(`<p class='user-info'>${l} ${response[l]}</p>`);
+                //      }
+                //
+                //
+                // });
 
                 console.log(response);
-                return response;
+                resolve (response);
             },
             error: function(err) {
                 return err;
             },
         });
+      });
+
     }
 
     _getLanguages(repoUrl) {
--- public/javascripts/handler.js
+++ public/javascripts/handler.js
@@ -1,64 +1,83 @@
 /*jshint esversion: 6*/
 const github = new GitHubApi("https://api.github.com/users/");
 //const Chart = require('chart.js');
-
-
-$(document).ready( () => {
-  $('.userinfo').on('click', (e) => {
-
-    const user = $(e.currentTarget).prev().html();
-    github.getUserInfo(user);
-    //github.getRepoLanguages(user);
-    $('#myChart').remove();
-    $('#repositories').append(' <canvas id="myChart" width="300" height="300"></canvas>');
-    let promiseArray =
-    github.getRepoLanguages(user).then((response) =>{
-      let languageLabel = Object.keys(response);
-      let languageData = Object.values(response);
-      console.log(languageLabel);
-      drawChart(languageLabel, languageData);
+function appendInfo(user) {
+    github.getUserInfo(user).then((response) => {
+        infoSelector(response);
 
     });
+}
 
+function drawChart(label, data) {
+    let chart = document.getElementById("myChart");
+    //chart.width(200);
+    var myDoughnutChart = new Chart(chart, {
+        type: 'doughnut',
+        data: {
+            labels: label,
+            datasets: [{
+                data: data,
+                backgroundColor: [
+                    '#6E352C',
+                    "#CF5230",
+                    "#F59A44",
+                    "#E3C598",
+                    "#8A6E64",
+                    "#6E612F"
+                ],
+                hoverBackgroundColor: [
+                    "#FF6344",
+                    "#36A2EB",
+                    "#FF6384",
+                    "#FFCE56"
+                ],
+                borderWidth: 0
+            }]
+        },
+        options: {
+            responsive: false,
 
-    function drawChart(label, data){
-      let chart = document.getElementById("myChart");
-      //chart.width(200);
-      var myDoughnutChart = new Chart(chart, {
-      type: 'doughnut',
-      data: {
-      labels: label,
-      datasets: [
-          {
-              data: data,
-              backgroundColor: [
-                  '#6E352C',
-                  "#CF5230",
-                  "#F59A44",
-                  "#E3C598",
-                  "#8A6E64",
-                  "#6E612F"
-              ],
-              hoverBackgroundColor: [
-                  "#FF6344",
-                  "#36A2EB",
-                  "#FF6384",
-                  "#FFCE56"
-              ],
-              borderWidth: 0
-          }]
-  },
-      options: {
-        responsive: false,
+            scale: {
+                display: false,
+            }
+        }
+    });
+}
 
-              scale: {
-                display:false,
-              }
-      }
-  });
-    }
+function infoSelector(info) {
+    let keys = Object.keys(info);
 
+    keys.forEach(function(l) {
+        console.log(l + " " + info[l]);
+        switch (l) {
+            case "name":
+                $('#repositories').append(`<h5>${l}</h5> <p class='user-info'>${info[l]}</p>`);
 
-  });
+                break;
+            case "html_url":
+                $('#repositories').append(`<h5>${l}</h5> <p class='user-info'>${info[l]}</p>`);
 
+                break;
+            case 'public_repos':
+                $('#repositories').append(`<h5>${l}</h5> <p class='user-info'>${info[l]}</p>`);
+
+                break;
+        }
+    });
+}
+$(document).ready(() => {
+    $('.userinfo').on('click', (e) => {
+        const user = $(e.currentTarget).prev().html();
+
+        $('#myChart').remove();
+        appendInfo(user);
+
+        $('#repositories').append(' <canvas id="myChart" width="300" height="300"></canvas>');
+        let promiseArray =
+            github.getRepoLanguages(user).then((response) => {
+                let languageLabel = Object.keys(response);
+                let languageData = Object.values(response);
+                drawChart(languageLabel, languageData);
+            });
+    });
 });
